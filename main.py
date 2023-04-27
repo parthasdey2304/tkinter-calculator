a44e7c4cbde17e0abfb5e0700248bf838725cd83
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-04-27 04:37:02 -0400

    calculator has been updated

--- main.py
+++ main.py
@@ -1,56 +1,87 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
-root.title("Calculator")
-root.geometry("300x300")
+class Calculator:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Calculator")
 
-# create a frame that has the entire length covered
-frame = Frame(root , bg = "grey", borderwidth = 5, relief = SUNKEN)
-frame.pack(side = TOP, fill = X)
+        # Create entry widget
+        self.entry = tk.Entry(self.master, width=25, justify="right")
+        self.entry.grid(row=0, column=0, columnspan=4)
 
-# create a frame that has the entire length and width covered
-frame2 = Frame(root , bg = "grey", borderwidth = 5, relief = SUNKEN)
-frame2.pack(side = TOP, fill = BOTH, expand = True)
+        # Create buttons for numbers
+        self.buttons = []
+        for i in range(10):
+            button = tk.Button(self.master, text=str(i), width=5, command=lambda x=i: self.button_click(x))
+            self.buttons.append(button)
 
-# there is an entry widget inside the first frame
-e = Entry(frame, width = 35, borderwidth = 5)
-e.pack(side= TOP, fill = X)
+        # Place number buttons in grid
+        for i in range(1, 10):
+            row = 3 - (i - 1) // 3
+            col = (i - 1) % 3
+            self.buttons[i].grid(row=row, column=col)
+        self.buttons[0].grid(row=4, column=0)
 
-# there are buttons of numbers, operators and clear button inside the second frame
-b1 = Button(frame2, text = "1", padx = 40, pady = 20)  
-b2 = Button(frame2, text = "2", padx = 40, pady = 20)
-b3 = Button(frame2, text = "3", padx = 40, pady = 20)
-b4 = Button(frame2, text = "4", padx = 40, pady = 20)
-b5 = Button(frame2, text = "5", padx = 40, pady = 20)
-b6 = Button(frame2, text = "6", padx = 40, pady = 20)
-b7 = Button(frame2, text = "7", padx = 40, pady = 20)
-b8 = Button(frame2, text = "8", padx = 40, pady = 20)
-b9 = Button(frame2, text = "9", padx = 40, pady = 20)
-b0 = Button(frame2, text = "0", padx = 40, pady = 20)
-b_add = Button(frame2, text = "+", padx = 40, pady = 20)
-b_sub = Button(frame2, text = "-", padx = 40, pady = 20)
-b_mul = Button(frame2, text = "*", padx = 40, pady = 20)
-b_div = Button(frame2, text = "/", padx = 40, pady = 20)
-b_equal = Button(frame2, text = "=", padx = 40, pady = 20)
-b_clear = Button(frame2, text = "Clear", padx = 40, pady = 20)
+        # Create operator buttons
+        self.add_button = tk.Button(self.master, text="+", width=5, command=lambda: self.operator_click("+"))
+        self.add_button.grid(row=1, column=3)
+        self.sub_button = tk.Button(self.master, text="-", width=5, command=lambda: self.operator_click("-"))
+        self.sub_button.grid(row=2, column=3)
+        self.mul_button = tk.Button(self.master, text="*", width=5, command=lambda: self.operator_click("*"))
+        self.mul_button.grid(row=3, column=3)
+        self.div_button = tk.Button(self.master, text="/", width=5, command=lambda: self.operator_click("/"))
+        self.div_button.grid(row=4, column=3)
 
-# here all the buttons are packed using grid
-b1.grid(row = 0, column = 0)
-b2.grid(row = 0, column = 1)
-b3.grid(row = 0, column = 2)
-b_add.grid(row = 0, column = 3)
-b4.grid(row = 1, column = 0)
-b5.grid(row = 1, column = 1)
-b6.grid(row = 1, column = 2)
-b_sub.grid(row = 1, column = 3)
-b7.grid(row = 2, column = 0)
-b8.grid(row = 2, column = 1)
-b9.grid(row = 2, column = 2)
-b_mul.grid(row = 2, column = 3)
-b0.grid(row = 3, column = 0)
-b_clear.grid(row = 3, column = 1)
-b_equal.grid(row = 3, column = 2)
-b_div.grid(row = 3, column = 3)
+        # Create clear button
+        self.clear_button = tk.Button(self.master, text="C", width=5, command=self.clear)
+        self.clear_button.grid(row=4, column=1)
 
+        # Create equal button
+        self.equal_button = tk.Button(self.master, text="=", width=5, command=self.calculate)
+        self.equal_button.grid(row=4, column=2)
 
+        self.num1 = ""
+        self.num2 = ""
+        self.operator = ""
+
+    def button_click(self, number):
+        if self.operator == "":
+            self.num1 += str(number)
+            self.entry.delete(0, tk.END)
+            self.entry.insert(0, self.num1)
+        else:
+            self.num2 += str(number)
+            self.entry.delete(0, tk.END)
+            self.entry.insert(0, self.num2)
+
+    def operator_click(self, operator):
+        self.operator = operator
+
+    def clear(self):
+        self.num1 = ""
+        self.num2 = ""
+        self.operator = ""
+        self.entry.delete(0, tk.END)
+
+    def calculate(self):
+        if self.num1 == "" or self.num2 == "" or self.operator == "":
+            return
+
+        num1 = float(self.num1)
+        num2 = float(self.num2)
+
+        if self.operator == "+":
+            result = num1 + num2
+        elif self.operator == "-":
+            result = num1 - num2
+        elif self.operator == "*":
+            result = num1 * num2
+        elif self.operator == "/":
+            result = num1 / num2
+
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, result)
+
+root = tk.Tk()
+calc = Calculator(root)
 root.mainloop()
